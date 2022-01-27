e6b0271ec8a9735c390cfd235724dbab800347c2
alphakilo11 <83025486+alphakilo11@users.noreply.github.com>
2022-01-27 14:38:56 +0100

    Create large_numbers.py

---
+++ large_numbers.py
@@ -0,0 +1,5 @@
+seconds_in_a_day = 24 * 60 * 60
+seconds_since_bigbang = seconds_in_a_day * 365.25 * 13.81e9
+meters_diameter_universe = 45e9 * 2 * 299792458 * 365.25 * seconds_in_a_day
+print(seconds_since_bigbang)
+meters_diameter_universe
