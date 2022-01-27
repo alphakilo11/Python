b5586dae94c1838c6d0a107e006bf0168753cd9c
alphakilo11 <83025486+alphakilo11@users.noreply.github.com>
2022-01-27 14:53:38 +0100

    Update results.py

--- results.py
+++ results.py
@@ -1,13 +1,19 @@
 """
+BUG Precision ist in diesem Fall irreführend; werden lauter 7er geschossen, so wäre die Präzision 0, aber dies entspricht nicht der Realität.
 Klassen für verschiedene Wettbewerbsmode verwenden?
+tag results, sodass ein Programm weiß welche Werte die Ergebnisse sind?
 (Datum YYYYMMDD, Wettbewerbs-ID GUID, Schützen-ID GUID, Scores)
 """
 
+import statistics
 
-results = ((20220127, 10, 10, 9, 8, 9, 10, 10),
+results = ((20220127, 7, 7, 7, 7, 7, 7, 7),
            (20220127, 10, 10, 9, 8, 9, 4, 10),
            (20220127, 10, 10, 9, 8, 5, 10, 10),
            (20220127, 10, 10, 9, 8, 7, 10, 10),
            (20220127, 10, 10, 9, 8, 9, 10, 10))
 
-results[0][5]
+summe = 0
+for i in range(1, len(results[0])):
+  summe += results[0][i]
+print(f"Gesamtpunkte: {summe}, Trueness: {((len(results[0]) - 1) * 10) - summe} Precision: {statistics.pstdev(results[0][1:])}")
