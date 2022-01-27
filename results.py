2a91c4f3160d41c42eff868b15738d4d95813304
alphakilo11 <83025486+alphakilo11@users.noreply.github.com>
2022-01-27 14:18:27 +0100

    Create results.py

---
+++ results.py
@@ -0,0 +1,13 @@
+"""
+Klassen für verschiedene Wettbewerbsmode verwenden?
+(Datum YYYYMMDD, Wettbewerbs-ID GUID, Schützen-ID GUID, Scores)
+"""
+
+
+results = ((20220127, 10, 10, 9, 8, 9, 10, 10),
+           (20220127, 10, 10, 9, 8, 9, 4, 10),
+           (20220127, 10, 10, 9, 8, 5, 10, 10),
+           (20220127, 10, 10, 9, 8, 7, 10, 10),
+           (20220127, 10, 10, 9, 8, 9, 10, 10))
+
+results[0][5]
