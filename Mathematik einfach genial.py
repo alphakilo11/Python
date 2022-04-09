3ad25230f6bb18deb6e8aaa2f7969d6f666c6b66
alphakilo11 <83025486+alphakilo11@users.noreply.github.com>
2022-04-09 15:12:11 +0200

    debugging

--- Mathematik einfach genial.py
+++ Mathematik einfach genial.py
@@ -1,14 +1,13 @@
-# Gesucht ist die Anzahl der Arten eine natürliche Zahl (x) als Summe aufeinanderfolgender natürlicher Zahlen darstellen zu können
-# Der höchste Summand kann größer als x/2 sein (eg: 1 + 2 = 3) Vermutlich nicht größer als math.ceil(x/2).
+# Gesucht ist die Anzahl der Arten eine natuerliche Zahl (x) als Summe aufeinanderfolgender natuerlicher Zahlen darstellen zu koennen
+# Der hoechste Summand kann groeßer als x/2 sein (eg: 1 + 2 = 3) Vermutlich nicht groeßer als math.ceil(x/2).
 import math
 
-zu_teilende_zahl = 5
-for i in range(math.ceil(zu_teilende_zahl / 2), 0, -1):
-  if (2 * i - 1) == zu_teilende_zahl:
-    print("yes!")
-  if (3 * i - 3) == zu_teilende_zahl:
-    print("yes!")
-  if (4 * i - 6) == zu_teilende_zahl:
-    print("yes!")
-  if (5 * i - 10) == zu_teilende_zahl:
-    print("yes!")
+zu_teilende_zahl = 18
+iterationen = 0
+for i in range(math.ceil(zu_teilende_zahl / 2), 0, -1): # hoechster Summand
+  for j in range(2, 6, 1): # Anzahl der Summanden
+    iterationen += 1
+    print(f"{j} * {i} - {dreieckszahl(j)} = {(j * i - dreieckszahl(j))}")
+    if (j * i - dreieckszahl(j)) == zu_teilende_zahl:
+      print("yes!")
+print(iterationen)
