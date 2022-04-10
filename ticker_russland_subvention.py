58bd51435f96890136b3aa24e10dfe36a781c89e
alphakilo11 <83025486+alphakilo11@users.noreply.github.com>
2022-04-10 20:00:57 +0200

    update

--- ticker_russland_subvention.py
+++ ticker_russland_subvention.py
@@ -1,9 +1,7 @@
 # money payed to russia since 24feb2022 (Beginn der Invasion der Ukraine)
-# TODO omit new lines in loop
 # TODO create ticker effect
 import datetime
 import time
-import calendar
 
 angriffsbeginn_utc = datetime.datetime(2022, 2, 24, 2, 00)
 ab_utc = time.struct_time((2022, 2, 24, 2, 0, 0, 0, 0 ,0)) # seconds since epoch
@@ -18,6 +16,9 @@ print(f"Zeit seit Angriffsbeginn: {dauer}")
 print(f"Zeit von Angriffsbeginn bis Statement: {dauer_angriffsbeginn_statement}")
 print(f"Tägliche Zahlungen der EU an Russland: {amount_per_day}")
 print(f"Zahlungen der EU an Russland seit Beginn der Invasion: € {round(amount_per_day * dauer.days)}")
-for i in range(10):
-  print(f"Zahlungen seit Beginn des Programms: {round((amount_per_day / 24 / 3600) * (time.time() - calendar.timegm(ab_utc)))} €")
+print(f"Zahlungen seit Beginn der Invasion: ", end="")
+while 1:
+  zahlungen = round((amount_per_day / 24 / 3600) * (time.time() - calendar.timegm(ab_utc)))
+  print(f"{zahlungen} €", end="")
   time.sleep(1)
+  print("\b" * (len(str(zahlungen)) + 2), end="") # replace value
