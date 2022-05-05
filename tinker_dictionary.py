fc62a981b161f7014dc14c4386dcfa270473ba03
Alexander <krendl.alexander@gmail.com>
2022-05-05 21:14:58 +0200

    improved

--- tinker_dictionary.py
+++ tinker_dictionary.py
@@ -6,7 +6,14 @@ with open('test.txt') as json_file:
 #    print(data)
 #    pretty print
 #    print(json.dumps(data, indent=4))
-print(len(data))
+# print(len(data))
+#for i, k in data.items():
+#    for j in range(10):
+#        print(k[j])
 for i, k in data.items():
-    for j in range(10):
-        print(k[j])
+    print("-" * 20, "START", "-" * 20)
+    print(i)
+    print("-" * 20)
+    for j in k[0].items():
+        print(j[1] / 1000) # this actually prints the degree C
+    print("-" * 20, "END", "-" * 20)
