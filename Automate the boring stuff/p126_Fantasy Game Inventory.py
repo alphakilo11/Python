9de927045ed42fd81aa30c403084e5a951f496fc
alphakilo11 <83025486+alphakilo11@users.noreply.github.com>
2022-05-07 10:53:44 +0200

    added addToInventory

--- Automate the boring stuff/p126_Fantasy Game Inventory.py
+++ Automate the boring stuff/p126_Fantasy Game Inventory.py
@@ -9,3 +9,18 @@ def displayInventory(inventory_dict):
     print("Total number of items:", total_items)
 
 displayInventory(inventory)
+
+dragonLoot = ['gold coin', 'gold coin', 'dagger', 'gold coin', 'ruby']
+
+def addToInventory(inventory, addedItems):
+    for i in addedItems:
+        if i in inventory:
+            old_value = inventory.get(i)
+            new_value = old_value + 1
+            inventory[i] = new_value
+        else:
+            inventory.setdefault(i, 1)
+    return inventory
+
+addToInventory(inventory, dragonLoot)
+displayInventory(inventory)
