59527f4875470e7a9a1f342c34a6df3fe49d2c27
Alexander <krendl.alexander@gmail.com>
2022-06-19 13:03:35 +0200

    new

--- collatz.py
+++ collatz.py
@@ -1,12 +1,12 @@
 import time
 start = time.time()
-for i in range(3,1000000):
-  #print(i, end=" ")
-  while i != 1:
-    if i % 2 == 0:
-      i = i / 2
-    else:
-      i = 3 * i + 1
-    #print(i, end=" ")
-  #print("")
+def collatz(ende):
+    for i in range(1, ende):
+        while i != 1:
+            if i % 2 == 0:
+                i = i / 2
+            else:
+                i = 3 * i + 1
+
+collatz(int(1e5))
 print(time.time() - start)
