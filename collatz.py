1462ab66db1804ed2a11ca42704174a4f9c6e264
alphakilo11 <83025486+alphakilo11@users.noreply.github.com>
2022-06-21 13:29:50 +0200

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -4,7 +4,7 @@ def collatz(ende):
     for i in range(1, ende):
         while i != 1:
             if i % 2 == 0:
-                i = int(i * 0.5) #int saves ca. 10% time
+                i = i * 0.5
             else:
                 i = 3 * i + 1
 
