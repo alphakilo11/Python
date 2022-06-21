976ac0a7dd7c78907355c40524b2ad37f9963cdc
Alexander <krendl.alexander@gmail.com>
2022-06-21 08:05:39 +0200

    faster

--- collatz.py
+++ collatz.py
@@ -4,9 +4,9 @@ def collatz(ende):
     for i in range(1, ende):
         while i != 1:
             if i % 2 == 0:
-                i = i / 2
+                i = int(i * 0.5) #int saves ca. 10% time
             else:
                 i = 3 * i + 1
 
-collatz(int(1e5))
+collatz(1000000)
 print(time.time() - start)
