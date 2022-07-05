277d6efdfd09438d52628c6a34a49d4ad4009076
Alexander <krendl.alexander@gmail.com>
2022-07-05 09:57:10 +0200

    new

---
+++ permutation.py
@@ -0,0 +1,31 @@
+# saves permutation variants to endergebnis variable
+
+from timeit import default_timer as timer
+
+def permutation(folge):
+    global ergebnis, level, endergebnis
+    if len(folge) > 1:
+        original = folge.copy()
+        for i in range(len(folge)):
+            ergebnis[level] = folge[i]
+            del folge[i]
+            level = level + 1
+            permutation(folge)
+            folge = original.copy()
+            if i == len(original) - 1:
+                level = level - 1
+    else:
+        ergebnis[level] = folge[0]
+        level = level - 1
+        endergebnis.append(ergebnis)
+        
+  
+folge = "abcdefghij"
+folge = [i for i in folge]
+ergebnis = [""] * len(folge)
+endergebnis = []
+level = 0
+startzeit = timer()
+permutation(folge)
+print("Dauer:", timer() - startzeit)
+print(len(endergebnis))
