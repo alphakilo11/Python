8286a7ab297893b2e4e1f1c3080b22cfeffd0ded
alphakilo11 <83025486+alphakilo11@users.noreply.github.com>
2022-07-20 17:25:41 +0200

    Update performance.py

--- performance.py
+++ performance.py
@@ -1,5 +1,18 @@
-import time
+from time import perf_counter_ns as timer
 import numpy as np
-start = time.perf_counter_ns()
+
+start = timer()
 foo = np.arange(int(1.5e7))
-print(time.perf_counter_ns() - start)
+print(timer() - start)
+del start, foo
+
+# file write performance
+import sys
+np.set_printoptions(threshold=sys.maxsize)
+foo = np.arange(int(1e5))
+start = timer()
+with open("AK_test_20220720_1.txt", "w") as file:
+  file.write(str(foo))
+print(timer() - start)
+del foo, start
+np.set_printoptions(threshold=None)
