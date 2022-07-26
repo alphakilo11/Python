d8dbb49a5a6d09e7449bfc369938230ecf33f8d7
alphakilo11 <83025486+alphakilo11@users.noreply.github.com>
2022-07-26 13:54:44 +0200

    Update ds18b20_temperatur.py

--- ds18b20_temperatur.py
+++ ds18b20_temperatur.py
@@ -3,10 +3,13 @@
 #	Note:
 #	Save it as seconds since epoch and convert it when accessing it (makes the file unreadable manually but makes it more flexible and saves memory)
 #----------------------------------------------------------------
+#BUG if store_now is set manually  you have to make sure to manually  set it to false again after storing the data as requested
+#ENHANCE it can take up to time_between_measurements s until a manual store command is executed
 import os
 import time
 
 progress_update = True # print every measurement to CLI
+store_now = False # when externally set to true it will trigger saving the date on the next cycle
 time_between_measurements = 300 # s
 ds18b20 = ''
 
@@ -32,19 +35,30 @@ if __name__ == '__main__':
 		temperaturelist.append(["Epoch-Time", "Temperature * 1000"])
 
 		while True:
+			# check for updated code (eg to change store_now or time_between_measurements or the redefine functions)
+			try:
+				with open("update.py", "r") as file:
+					  code = file.read()
+				exec(code)
+				del code
+			except:
+				print("Could not read update.py - execution continues")
+			# read temperature
 			if read() != None:
 				step = [int(time.time()), read()]
 				temperaturelist.append(step)
 				if progress_update:
 					print(step, "next in", time_between_measurements, "s.")
+			# store data as scheduled
 			now = time.localtime(time.time())
-			if now[2] == next_save_day and now[3] >= 2: #triggers once a day after 2 o'clock 
+			if store_now or (now[2] == next_save_day and now[3] >= 2): #triggers once a day after 2 o'clock 
 				filename = str(now[0]) + str(now[1]) + str(now[2]) + "_temperatures.txt"
 				with open(filename, "w") as file:
 					file.write(str(temperaturelist))
-				temperaturelist = []
 				next_save_day = time.localtime(time.time() + 3600 * 24)[2]
 				print("Successfully saved to", filename, "Next save on", next_save_day,".")
+				store_now = False
+				temperaturelist = []
 				del filename, now
 
 			time.sleep(time_between_measurements) 
