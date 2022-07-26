50c01de6e8a0d25baa452c7da0db0d1f2dcb7f2c
alphakilo11 <83025486+alphakilo11@users.noreply.github.com>
2022-07-26 12:02:37 +0200

    restructure

--- ds18b20_temperatur.py
+++ ds18b20_temperatur.py
@@ -6,15 +6,9 @@
 import os
 import time
 
-progress_update = True
+progress_update = True # print every measurement to CLI
 time_between_measurements = 300 # s
 ds18b20 = ''
-
-def setup():
-	global ds18b20
-	for i in os.listdir('/sys/bus/w1/devices'):
-		if i != 'w1_bus_master1':
-			ds18b20 = '28-01204bd1256e'
 
 def read():
 #	global ds18b20
@@ -26,34 +20,35 @@ def read():
 	temperature = temperaturedata[2:]
 #	temperature = temperature / 1000 # 1000? this seems rather strange. I would expect a base 2 value.
 	return temperature
-	
-def loop():
-	global temperaturelist, next_save_day
-	while True: #while True
-		if read() != None:
-			step = [int(time.time()), read()]
-			temperaturelist.append(step)
-			if progress_update:
-				print(step, "next in", time_between_measurements, "s.")
-		now = time.localtime(time.time())
-		if now[2] == next_save_day and now[3] >= 2: #triggers once a day after 2 o'clock 
-			filename = str(now[0]) + str(now[1]) + str(now[2]) + "_temperatures.txt"
-			with open(filename, "w") as file:
-				file.write(str(temperaturelist))
-			temperaturelist = []
-			next_save_day = time.localtime(time.time() + 3600 * 24)[2]
-			print("Successfully saved to", filename, "Next save on", next_save_day,".")
-			del filename, now
-
-		time.sleep(time_between_measurements) 
 
 if __name__ == '__main__':
 	try:
 		temperaturelist = []
 		next_save_day = time.gmtime(time.time() + 3600 * 24)[2]
-		setup()
+		# setup
+		for i in os.listdir('/sys/bus/w1/devices'):
+			if i != 'w1_bus_master1':
+				ds18b20 = '28-01204bd1256e'
+				
 		temperaturelist.append(["Epoch-Time", "Temperature * 1000"])
-		loop()
+
+		while True:
+			if read() != None:
+				step = [int(time.time()), read()]
+				temperaturelist.append(step)
+				if progress_update:
+					print(step, "next in", time_between_measurements, "s.")
+			now = time.localtime(time.time())
+			if now[2] == next_save_day and now[3] >= 2: #triggers once a day after 2 o'clock 
+				filename = str(now[0]) + str(now[1]) + str(now[2]) + "_temperatures.txt"
+				with open(filename, "w") as file:
+					file.write(str(temperaturelist))
+				temperaturelist = []
+				next_save_day = time.localtime(time.time() + 3600 * 24)[2]
+				print("Successfully saved to", filename, "Next save on", next_save_day,".")
+				del filename, now
+
+			time.sleep(time_between_measurements) 
 	finally:
 		now = time.localtime(time.time())
 		filename = str(now[0]) + str(now[1]) + str(now[2]) + "_temperatures.txt"
