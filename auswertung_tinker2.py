696d83e9f6f07d5e11dd7a6c4c6bad51b7294c1c
Alexander <krendl.alexander@gmail.com>
2022-07-26 17:44:17 +0200

    blub

--- auswertung_tinker2.py
+++ auswertung_tinker2.py
@@ -1,6 +1,22 @@
+from matplotlib import pyplot as plt
+import time
+
 with open("temp_log2.txt") as file:
     data = file.read()
-cleanup_list = [" ", "["]
-foo = data.replace(" ", "")
-foo = foo.split("],")
-print(foo[1])
+cleanup_list = [" ", "[", "'"]
+for char in cleanup_list:
+    data = data.replace(char, "")
+data = data.split("],")
+
+x_values = []
+y_values = []
+for i in range(1, len(data) - 1):
+    step = data[i].split(",")
+    x_values.append(int(step[0]))
+    print(time.localtime(int(step[0]))[3],":", time.localtime(int(step[0]))[4], int(step[0]))
+    y_values.append(int(step[1]))
+
+plt.plot(x_values, y_values, '-')
+
+plt.title("Temperature Histogramm")
+plt.show()
