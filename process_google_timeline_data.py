417617ef41d7c37787ca7fbacde71b7bee0d2aa9
alphakilo11 <83025486+alphakilo11@users.noreply.github.com>
2022-11-29 17:41:42 +0100

    Create process_google_timeline_data.py

---
+++ process_google_timeline_data.py
@@ -0,0 +1,14 @@
+import json
+"""
+open timeline data and show the timestamps of all activities exceeding the cutoff travel distance
+"""
+cutoff_distance = 100000
+
+with open('2022_JUNE.json', 'r') as file:
+    dict = json.load(file)
+for i in range(len(dict["timelineObjects"])):
+    if 'activitySegment' in dict["timelineObjects"][i]:
+        if 'distance' in dict["timelineObjects"][i]["activitySegment"]:
+            distance = dict["timelineObjects"][i]["activitySegment"]['distance']
+            if distance > cutoff_distance:
+                print(distance, dict["timelineObjects"][i]["activitySegment"]["duration"]["startTimestamp"])
