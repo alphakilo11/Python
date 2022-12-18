84afeb3742d3f1843ec50c23e2b94c51933156bc
alphakilo11 <83025486+alphakilo11@users.noreply.github.com>
2022-12-18 20:09:52 +0100

    Update ABE_Auswertung.py

--- ABE_Auswertung.py
+++ ABE_Auswertung.py
@@ -1,27 +1,68 @@
-def ABE_auswertung(folderpath):
+from google.colab import drive
+drive.mount('/content/drive')
+
+def ABE_auswertung(folderpath='/content/drive/MyDrive/ArmA 3/Homebrew/Automated Battle Engine/Results_1', source_file_path='/incoming', source_file_type='.rpt'):
   """
   extracts Automated Battle Engine results from Arma 3 rpt files
+
   Example:
     print(ABE_auswertung("/content/drive/MyDrive/ArmA 3/Homebrew/Automated Battle Engine/Results_1"))
   ENHANCE use regex
   ENHANCE include datetime (filename + timestamp) - but what's the benefit?
   ENHANCE ignore other files
   """
-  from os import listdir
-  from os.path import isfile, join
-  file_list = [f for f in listdir(folderpath) if isfile(join(folderpath, f))]
-  step1 = ''
-  for file in file_list:
-    with open((folderpath + '/' + file), 'r') as datei:
-      step1 += datei.read()
-  step1 = step1.split('\n')
-  # get Result Lines
-  step2 = []
-  for line in step1:
-    if 'AKBL Result: Survivors:' in line:
-      step2.append(line)
-  del step1
+  from os import listdir, makedirs
+  from os.path import isfile, join, exists
+  import shutil
+
+  # check for source_file_type files in source_file_path folder
+  file_list = [f for f in listdir(folderpath + source_file_path) if isfile(join(folderpath + source_file_path, f))]
+  if len(file_list) > 0: 
+    skipped_files = 0
+    akbl_results = []
+    for file in file_list:
+      if file[-4:] == source_file_type:
+        with open((folderpath + source_file_path + '/' + file), 'r') as datei:
+          step1 = datei.readlines()
+        step2 = []
+        for line in step1:
+          if 'AKBL' in line:
+            if 'AKBL Result: Survivors:' in line:
+              date = file[-23:-13] + ' ' # convert filename to timestamp
+              akbl_results.append(date + line)
+              del date 
+            step2.append(line)
+        #create folders if necessary
+        newpath = folderpath + '/archive' 
+        if not exists(newpath):
+            makedirs(newpath)
+        newpath = folderpath + '/archive/AKBL_archive' 
+        if not exists(newpath):
+            makedirs(newpath)
+        # create filtered AKBL files
+        try:
+          with open(folderpath + '/archive/AKBL_archive/' + file[:-4] + '.txt', 'x') as datei:
+            datei.writelines(step2)
+          # archive source_file_type file
+          shutil.move (folderpath + source_file_path + '/' + file, folderpath + '/archive/' + file)
+        except FileExistsError:
+          print(f"WARNING: {folderpath + '/archive/AKBL_archive/' + file[:-4] + '.txt'} already exists, which means most likely that the corresponding {source_file_type} has already been processed.")
+          skipped_files += 1
+        del step1, step2
+      else:
+        skipped_files += 1
+    # update the permanent file
+    if len(akbl_results) > 0:
+      with open(folderpath + '/AKBL_collected_results.txt', 'a') as file:
+        file.writelines(akbl_results)
+    print(f'{len(file_list) - skipped_files} files have been processed. {skipped_files} files have been skipped.')
+    del akbl_results, file_list, skipped_files
+  else:
+    print(f'No {source_file_type} files in {source_file_path}')
+
   # break apart Result Lines
+  with open(folderpath + '/AKBL_collected_results.txt', 'r') as file:
+    step2 = file.readlines()
   step3 = []
   for line in step2:
     step3.append(line.split('Survivors: ')[1])
@@ -37,6 +78,8 @@ def ABE_auswertung(folderpath):
 def ABE_aufbereitung(daten):
   """
   Soll die Daten übersichticher darstellen.
+  Example:
+    print(ABE_aufbereitung(ABE_auswertung("/content/drive/MyDrive/ArmA 3/Homebrew/Automated Battle Engine/Results_1")))
   """
   output = []
   for line in daten:
@@ -53,6 +96,7 @@ def ABE_aufbereitung(daten):
 
 def create_matrix(data):
   """
+  WIP
   Create a matrix
   Example:
     print(create_matrix(ABE_auswertung("/content/drive/MyDrive/ArmA 3/Homebrew/Automated Battle Engine/Results_1")))
