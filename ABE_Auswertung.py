640fe285295cd6a14109cbd73eaec499243f2bce
alphakilo11 <83025486+alphakilo11@users.noreply.github.com>
2022-12-30 12:18:21 +0100

    fix

--- ABE_Auswertung.py
+++ ABE_Auswertung.py
@@ -292,5 +292,5 @@ def create_matrix(data):
 #Execution
 file_operation("/content/drive/MyDrive/ArmA 3/Homebrew/Automated Battle Engine/Results_1")
 duration_report(battle_duration(break_apart()))
-types_to_eliminate(create_result_DataFrame(break_apart()).sort_values('score'))
+print(types_to_eliminate(create_result_DataFrame(break_apart())))
 create_result_DataFrame(break_apart()).sort_values('score')
