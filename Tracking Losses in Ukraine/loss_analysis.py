180335b8d71e7f9d6abd6977ab0593901ca9dbf9
alphakilo11 <83025486+alphakilo11@users.noreply.github.com>
2023-01-25 14:23:51 +0100

    qol

--- Tracking Losses in Ukraine/loss_analysis.py
+++ Tracking Losses in Ukraine/loss_analysis.py
@@ -36,32 +36,3 @@ def loss_analysis(filepath, variant=True):
         counter = result.get(temp.group('model'), 0)
         result[temp.group('model')] = counter + int(temp.group('number'))
     return result
-
-spam = loss_analysis('/content/test.txt', False)
-print(spam)
-print(sum(spam.values()) + 180)
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-# Fixing random state for reproducibility
-np.random.seed(19680801)
-
-
-plt.rcdefaults()
-fig, ax = plt.subplots()
-
-# Example data
-people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
-y_pos = np.arange(len(people))
-performance = 3 + 10 * np.random.rand(len(people))
-error = np.random.rand(len(people))
-
-ax.barh(y_pos, performance, xerr=error, align='center')
-ax.set_yticks(y_pos)
-ax.set_yticklabels(people)
-ax.invert_yaxis()  # labels read top-to-bottom
-ax.set_xlabel('Performance')
-ax.set_title('How fast do you want to go today?')
-
-plt.show()
