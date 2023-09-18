bf274ae672f7563f0119fef47cd462e387e7075f
alphakilo11 <83025486+alphakilo11@users.noreply.github.com>
2023-09-18 21:27:13 +0200

    Kapitel 1-3

--- statistik.py
+++ statistik.py
@@ -2,3 +2,13 @@
 def relative_haeufigkeit(absolute_haeufigkeit, grundgesamtheit):
     return absolute_haeufigkeit / grundgesamtheit
 
+def haeufigkeitsdichte(relative_haeufigkeit, klassenbreite):
+    """S 47 Säulenhöhe in einem Histogramm."""
+    return relative_haeufigkeit / klassenbreite
+
+def klassenmitte(untergrenze, obergrenze):
+    """S 50"""
+    return (untergrenze + obergrenze) / 2
+
+# Kapitel 4 Lagemaßzahlen
+
