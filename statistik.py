10d6037306501708aedd3bf3de6d3b15813d63c9
alphakilo11 <83025486+alphakilo11@users.noreply.github.com>
2023-09-18 21:06:56 +0200

    first

---
+++ statistik.py
@@ -0,0 +1,4 @@
+""" This Module is based on the book "Fit fürs Studium Statistik" by Benno Grabinger, 2. korrigierte Auflage 2021 """
+def relative_haeufigkeit(absolute_haeufigkeit, grundgesamtheit):
+    return absolute_haeufigkeit / grundgesamtheit
+
